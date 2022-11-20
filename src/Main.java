8d6b1eeb0bb81b415463be5265796c41a47a56dd
yash <yashbhokare1192@gmail.com>
2022-11-20 22:54:55 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+import java.util.Scanner;
+//public class Main{
+//import java.util.Scanner;
+  //  public static void main(String[] args){
+//         System.out.println("Helllnnno world yash////");
+// int a=0;
+// int yash=1;
+// int b=2;
+// int c;
+// c=a+b+yash;
+// System.out.println(c);
+// //int i_111;
+// user input
+
+   /*     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number 1 ");
+    int a = sc.nextInt();
+    System.out.println("Enter number 2 ");
+    int b = sc.nextInt();
+    int sum = a+b;
+    System.out.println("The sum of a and b is :");
+    System.out.println(sum);
+*/
+/*int age=233;
+System.out.println(age);
+
+char letter='w';
+        System.out.println(letter);
+        boolean isage=true;
+        System.out.println(isage);
+double yash=1393939;
+System.out.println(yash);
+        //System.out.print(yash);
+float arr = 122132.777F;
+        System.out.println(arr);
+  */
+  /*String name="yash";
+  String surname=name;
+  System.out.println(name);
+  System.out.println(name.length());
+  System.out.println(name.charAt(2) );
+  System.out.println(surname);*/
+//  System.out.println()
+
+      /*  int[] marks = new int[3];
+        marks[0] = 89;
+        marks[1]=90;
+        marks[2]=44;
+
+        System.out.println(marks[1]);
+       //length
+
+        //sort
+
+Arrays.sort(marks);*/
+
+//pratice
+
+
+       /* Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+        Expected Output :
+        Hello
+        Alexandra Abramov */
+
+    public class Main{
+    public static void main(String[] args);{
+
+            System.out.println("Hello ");
+            System.out.println("Alexandra Abramov");
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
